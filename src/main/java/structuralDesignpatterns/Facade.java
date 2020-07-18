3841b06b0a2cf79d3737350c54c0e33315628613
taeXtream <natdanai.ang@gmail.com>
2020-07-18 20:06:23 +0700

    Add Facade

---
+++ src/main/java/structuralDesignpatterns/Facade.java
@@ -0,0 +1,58 @@
+package structuralDesignpatterns;
+
+public class Facade {
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
+
+interface Shape {
+    void draw();
+}
+
+class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
+
+class Square implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing Square");
+    }
+}
+
+class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class ShapeMaker {
+    private final Shape circle;
+    private final Shape rectangle;
+    private final Shape square;
+
+    public ShapeMaker() {
+        circle = new Circle();
+        rectangle = new Rectangle();
+        square = new Square();
+    }
+
+    public void drawCircle(){
+        circle.draw();
+    }
+    public void drawRectangle(){
+        rectangle.draw();
+    }
+    public void drawSquare(){
+        square.draw();
+    }
+}
