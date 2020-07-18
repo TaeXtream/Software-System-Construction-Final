189b98300aba9777038fcae9c74c3cc81a6e0bc1
taeXtream <natdanai.ang@gmail.com>
2020-07-18 20:38:43 +0700

    Add Chain of responsibility

---
+++ src/main/java/behavioralDesignpatterns/Chain.java
@@ -0,0 +1,140 @@
+package behavioralDesignpatterns;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Objects;
+
+public class Chain {
+    public static void main(String[] args) {
+        OrcKing king = new OrcKing();
+        king.makeRequest(new Request(RequestType.DEFEND_CASTLE, "defend castle"));
+        king.makeRequest(new Request(RequestType.TORTURE_PRISONER, "torture prisoner"));
+        king.makeRequest(new Request(RequestType.COLLECT_TAX, "collect tax"));
+    }
+}
+
+@Getter
+class Request {
+
+    private final RequestType requestType;
+    private final String requestDescription;
+    private boolean handled;
+
+    public Request(final RequestType requestType, final String requestDescription) {
+        this.requestType = Objects.requireNonNull(requestType);
+        this.requestDescription = Objects.requireNonNull(requestDescription);
+    }
+
+    public void markHandled() { this.handled = true; }
+
+    public boolean isHandled() { return this.handled; }
+
+    @Override
+    public String toString() { return getRequestDescription(); }
+}
+
+enum RequestType {
+    DEFEND_CASTLE, TORTURE_PRISONER, COLLECT_TAX
+}
+
+@AllArgsConstructor
+abstract class RequestHandler {
+    private final RequestHandler next;
+
+
+    public void handleRequest(Request req) {
+        if (next != null) {
+            next.handleRequest(req);
+        }
+    }
+
+    protected void printHandling(Request req) {
+        System.out.println(this.toString() + " handling request " + req.getRequestDescription());
+    }
+
+    @Override
+    public abstract String toString();
+}
+
+class OrcCommander extends RequestHandler {
+    public OrcCommander(RequestHandler handler) {
+        super(handler);
+    }
+
+    @Override
+    public void handleRequest(Request req) {
+        if (req.getRequestType().equals(RequestType.DEFEND_CASTLE)) {
+            printHandling(req);
+            req.markHandled();
+        } else {
+            super.handleRequest(req);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Orc Commander";
+    }
+}
+
+class OrcOfficer extends RequestHandler {
+
+    public OrcOfficer(RequestHandler next) {
+        super(next);
+    }
+
+    @Override
+    public void handleRequest(Request req) {
+        if (req.getRequestType().equals(RequestType.TORTURE_PRISONER)) {
+            printHandling(req);
+            req.markHandled();
+        } else {
+            super.handleRequest(req);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Orc Soldier";
+    }
+}
+
+class OrcSoldier extends RequestHandler {
+
+
+    public OrcSoldier(RequestHandler next) {
+        super(next);
+    }
+
+    @Override
+    public void handleRequest(Request req) {
+        if (req.getRequestType().equals(RequestType.COLLECT_TAX)) {
+            printHandling(req);
+            req.markHandled();
+        } else {
+            super.handleRequest(req);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Orc Officer";
+    }
+}
+
+class OrcKing {
+    RequestHandler chain;
+
+    public OrcKing() {
+        buildChain();
+    }
+
+    private void buildChain() {
+        chain = new OrcCommander(new OrcOfficer(new OrcSoldier(null)));
+    }
+
+    public void makeRequest(Request req) {
+        chain.handleRequest(req);
+    }
+}
